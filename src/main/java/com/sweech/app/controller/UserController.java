be5aaa3d25d41f012c16b699cea7a973c67f3667
mochebrolu <mochebrolu@coca-cola.com>
2025-05-30 00:16:52 +0530

    Update UserController.java

--- src/main/java/com/sweech/app/controller/UserController.java
+++ src/main/java/com/sweech/app/controller/UserController.java
@@ -1,36 +1,31 @@
 package com.sweech.app.controller;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sweech.app.dto.UserDto;
+import com.sweech.app.service.UserService;
+
+@RestController
+@RequestMapping("/api")
 public class UserController {
-    private String email;
-    private String password;
-    private String username;
-    private String registrationTime;
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getRegistrationTime() {
-		return registrationTime;
-	}
-	public void setRegistrationTime(String registrationTime) {
-		this.registrationTime = registrationTime;
-	}
 
-    
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(value = "/signup", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<?> signup(@RequestBody UserDto userDto) {
+        try {
+            userService.signup(userDto);
+            return ResponseEntity.ok(Map.of("id", userDto.getEmail(), "username", userDto.getUsername(), "registrationTime", userDto.getRegistrationTime()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "signup_failed", "message", e.getMessage()));
+        }
+    }
 }
-
-
